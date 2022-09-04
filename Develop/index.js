d0d8a3c42ed7104d56a5be3c81cb72fb70d42a19
Jeremy Lin <maxlinjr95@gmail.com>
2022-09-04 15:41:57 -0700

    minor changes

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,9 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("until");
+const util = require("utils");
 
-const generateMarkdown = ("./utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [{
