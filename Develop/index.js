08fc669c8a3dbbc5cc5cedf743971b6f6f8020f3
Jeremy Lin <maxlinjr95@gmail.com>
2022-09-04 15:30:54 -0700

    questions added

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,44 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("until");
+
+const generateMarkdown = ("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    message: "What is the project's title?",
+    name: "Title"
+}, {
+    type: "input",
+    message: "Enter a description",
+    name: "Table of Contents"
+}, {
+    type: "input",
+    message: "What are the installation instructions?",
+    name: "Installation"
+}, {
+    type: "input",
+    message: "What is this used for?",
+    name: "Usage"
+}, {
+    type: "input",
+    message: "What licenses are being used?",
+    name: "License"
+}, {
+    type: "input",
+    message: "What are the contribution guidelines?",
+    name: "Contribution"
+}, {
+    type: "input",
+    message: "What tests are being used?",
+    name: "Tests"
+}, {
+    type: "input",
+    message: "For any additional questions, contact:",
+    name: "Questions"
+}, {
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
