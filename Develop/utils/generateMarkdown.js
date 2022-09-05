a3de00f81c690cd97a41ed1656ef9b6eb4a1f002
Jeremy Lin <maxlinjr95@gmail.com>
2022-09-05 14:52:11 -0700

    table of contents added

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -51,7 +51,16 @@ function generateMarkdown(data) {
     ${answers.description}
 
   ## Table of Contents
-    [Description]()
+    [Description](#description)
+    [Installation](#installation)
+    [Usage](#usage)
+    [License](#license)
+    [Contributing](#contributing)
+    [Tests](#tests)
+    [Questions](#questions)
+
+  ## Installation
+    ${answers.installation}
 
 `;
 }
