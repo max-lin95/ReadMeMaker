b656ddfa542d451a75fbca82f4c64828382d8394
Jeremy Lin <maxlinjr95@gmail.com>
2022-09-05 14:46:03 -0700

    additional license functions added

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -32,11 +32,26 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = "";
+
+  if (license != "None") {
+    licenseSection += "## License"
+    licenseSection += "Refer to " + renderLicenseSection(license) + " for more information.";
+
+  }
+  return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ## Description
+    ${answers.description}
+
+  ## Table of Contents
+    [Description]()
 
 `;
 }
